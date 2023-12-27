8c8865eb0cd3ca15867a0478eab68b685634e69e
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-12-27 16:40:40 -0600

    it was createted the repository

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { AppDataSource } from "./data-source"
+import { User } from "./entity/User"
+
+AppDataSource.initialize().then(async () => {
+
+
+
+}).catch(error => console.log(error))
+
+
+const port = 3000
